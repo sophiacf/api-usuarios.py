3cdedd889932e938b73faf83c340c912f397d6af
Sophia Cara <189317785+sophiacf@users.noreply.github.com>
2026-05-15 14:35:32 -0300

    Update app.py

--- app.py
+++ app.py
@@ -25,17 +25,54 @@ usuarios = [
 def home():
     return jsonify({"mensagem": "API de usuarios - Acesse /usuarios"})
 
+
 @app.route("/usuarios", methods=["GET"])
 def listar_usuarios():
     return jsonify(usuarios)
 
+
 @app.route("/usuarios", methods=["POST"])
 def criar_usuario():
+
     novo = request.json
+
     novo['id'] = len(usuarios) + 1
+
     usuarios.append(novo)
 
     return jsonify(novo), 201
+
+
+
+@app.route("/usuarios/<int:id>", methods=["PUT"])
+def atualizar_usuario(id):
+
+    for usuario in usuarios:
+
+        if usuario["id"] == id:
+
+            dados = request.json
+
+            usuario["nome"] = dados["nome"]
+
+            return jsonify(usuario)
+
+    return jsonify({"erro": "Usuário não encontrado"}), 404
+
+
+@app.route("/usuarios/<int:id>", methods=["DELETE"])
+def deletar_usuario(id):
+
+    for usuario in usuarios:
+
+        if usuario["id"] == id:
+
+            usuarios.remove(usuario)
+
+            return jsonify({"mensagem": "Usuário deletado com sucesso"})
+
+    return jsonify({"erro": "Usuário não encontrado"}), 404
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT" , 5000))
